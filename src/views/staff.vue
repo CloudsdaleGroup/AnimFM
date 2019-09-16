0f629fce80cb6c0ce6538625f2252b04ae168d8f
Asthriona <im@asthriona.com>
2019-09-16 06:13:45 +0200

    add staff page

--- src/views/staff.vue
+++ src/views/staff.vue
@@ -1,7 +1,73 @@
 <template>
     <section class="staff">
     <TheNav />
-        <h1>STAFF PAGE</h1>
+    <div class="container">
+  <div class="row">
+    <div class="mx-auto">
+      <p>&nbsp;</p>
+<img class="img-fluid mx-auto d-block rounded-circle" src="https://pbs.twimg.com/profile_images/1162346680198606849/SpZ_tXdk_400x400.png" width="150px" height="150px" />
+    <h2>Asthriona</h2>
+        <h4>Co-Founder, Lead Producer</h4>
+    </div>
+        <div class="row">
+          <div class="col-sm-8 mx-auto text-justify">
+            <p>
+              Hi! I'm Asthriona, CloudsdaleGroup's Dev Lead and Sirius Media Group's Hardware/Editor/Producer/ Datacenter Manager, I'm one of the Founder of AnimeFM,<br>
+              I recently join CloudsdaleGroup to give a hand and manage stuff. that's one of the thing I do the best.<br>
+              I'm faceless and Nameless, not I dont like who I'am but... I definitly dont like who I'am. You maybe know this, but I'm no one, so no school stuff, no past, no future.<br>
+              That's it for me :)
+            </p>
+            <p class="links">
+              His links: 
+                <a href="https://twitter.com/Asthriona">Twitter</a> | <a href="MAILTO:Asthriona@CloudsdaleGroup.com">Mail</a>
+            </p>
+            <hr/>
+          </div>
+        </div>
+              <div class="mx-auto">
+                <img class="img-fluid mx-auto d-block rounded-circle" src="http://cdn.siriushd.net/raven%20icon.png" width="150px" height="150px" />
+                <h2>Killian "Azmerhos" Patriowski</h2>
+                <h4>Infrastructure Administrator, Co-Founder</h4>
+              </div>
+        <div class="row">
+          <div class="col-sm-8 mx-auto text-justify">
+            <p>
+            I am Killian Patriowski, interested in everything about machines, but also the labyrinth that can be the network and its security, and incidentally a farmer player but bad ^^ "
+            Future multi-game streamer, I can find myself on Osu! in standard mode, soon on WoW, Minecraft, and also on Warframe, LoL and Overwatch or Paladins.
+            I'm still studying and learning with Sirius to maintain a network and these machines, the Cloudsdale system in this case, and expects to end up holding my personal network myself. In my spare time, I take care of communicating and correcting the various texts of Cloudsdale and Sirius Media Group on demand.
+            Finally, I am one of the moderators twitch, YouTube and discord of Sirius and take care of listing and testing the numbers of false technical service.
+            </p>
+            <p class="links">
+              Ses liens: 
+                <a href="https://twitter.com/azmerhos">Twitter</a> | <a href="MAILTO:azmerhos@CloudsdaleGroup.com">Mail</a>
+            </p>
+            <hr>
+            </div>
+        </div>
+
+        <div class="mx-auto">
+                <img class="img-fluid mx-auto d-block rounded-circle" src="https://www.siriusmediagroup.com/themes/sirius-media-group/assets/img/sTqhVLv__400x400.jpg" width="150px" height="150px" />
+                <h2>Quentin Silva</h2>
+                <h4>Co-Founder, CloudsdaleGroup CEO</h4>
+              </div>
+        <div class="row">
+          <div class="col-sm-8 mx-auto text-justify">
+            <p>
+            Hey, I'm Sirius the founder of Cloudsdale Radio & AnimeFM, I started the Cloudsdale Radio project in 2013 as a radio enthusiast and a technical challenge too. 
+            I was, in time, studying telecommunications & networks. This is where I started having my first servers (VPS to start). 
+            Basically, the goal was to create a functional radio 24/7 (or 247 as I like to say) that is automated at best. Then, as I went along, 
+            I became attached to the project in such a way that I could not stop it, although there were moments of emptiness, moments when I got completely tired of it. 
+            project, I am obliged to maintain it. Today Cloudsdale Radio is an infrastructure almost independent of 6 servers.
+            </p>
+            <p class="links">
+              His links: 
+                <a href="https://twitter.com/Sirius242">Twitter</a> | <a href="MAILTO:QuentinSilva@CloudsdaleGroup.com">Mail</a>
+            </p>
+            </div>
+        </div>
+  </div>
+</div>
+
     </section>
 </template>
 
@@ -16,3 +82,8 @@ export default {
     },
 }
 </script>
+<style scoped>
+.links {
+    text-align: center;
+}
+</style>

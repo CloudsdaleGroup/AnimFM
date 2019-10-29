6442693ade4bb2c9d3249351e714f262d809e03d
Asthriona <im@asthriona.com>
2019-10-29 11:54:25 +0100

    Add Cloudsdale radio popup

---
+++ src/components/ThePlayerCR.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="playerCR">
+        <h1>Cloudsdale Radio</h1>
+        <hr>
+        <iframe src="http://code02.cloudsdaleradio.com/public/cloudsdale_radio/embed" frameborder="0" allowtransparency="true" style="width: 50%; min-height: 150px; border: 0;"></iframe>
+    </div>
+</template>
+
+<style scoped>
+    iframe {
+        background-color: rgba(255, 255, 255, 0.4);
+        box-shadow: 0px 0px 20px 2px rgba(255, 255, 255, 0.7);
+        border-radius: 5px;
+        height: 110px;
+    }
+    h1 {
+        color: aliceblue;
+        background: rgba(0, 0, 0, 0.6);
+        margin-left: 15%;
+        margin-right: 15%;
+        border-radius: 10px;
+    }
+
+</style>

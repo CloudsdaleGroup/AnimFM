74a1cb4fe0c03b3c4874029aeae76b4ae3bbba77
Asthriona <im@asthriona.com>
2019-10-29 11:54:14 +0100

    Update AnimeFM popup (temp)

--- src/components/ThePlayer.vue
+++ src/components/ThePlayer.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="player">
-        <iframe src="http://code02.cloudsdaleradio.com/public/cloudsdale_radio/embed" frameborder="0" allowtransparency="true" style="width: 50%; min-height: 150px; border: 0;"></iframe>
+        <h1>AnimeFM</h1>
+        <hr>
+        <iframe src="http://code02.cloudsdaleradio.com/public/animefm/embed" frameborder="0" allowtransparency="true" style="width: 50%; min-height: 150px; border: 0;"></iframe>
     </div>
 </template>
 
@@ -10,6 +12,13 @@
         box-shadow: 0px 0px 20px 2px rgba(255, 255, 255, 0.6);
         border-radius: 5px;
         height: 110px;
-    
     }
+    h1 {
+        color: aliceblue;
+        background: rgba(0, 0, 0, 0.6);
+        margin-left: 15%;
+        margin-right: 15%;
+        border-radius: 10px;
+    }
+
 </style>

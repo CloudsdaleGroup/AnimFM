d3c7f05e9cb77caae96390a5cbcb682bff535515
Asthriona <im@asthriona.com>
2019-10-29 11:54:35 +0100

    Add 404 page

---
+++ src/views/404.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="container">
+  <div class="row">
+      <TheNav />
+    <div class="mx-auto">
+      <h1 class="text-center"> Not Found </h1>
+      <h3>Did you do smth wrong ?</h3>
+      <p>The server say it dont find the page you ask for, We look everywhere and it seem like we lost it :/ Sorry :c<br>
+      Please try again or try another link, We recommend using the NavBar on the top of you screen. All the link has been verified!</p>
+      <p></p>
+      <img class="rounded" src="http://cdn.asthriona.com/v9iflkC.gif" alt="idk dude, bouge ta souris! :c">
+
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+img {
+    padding-bottom: 5px;
+}
+</style>
+
+<script>
+// @ is an alias to /src
+import TheNav from '@/components/TheNav.vue'
+
+export default {
+  name: '404',
+  components: {
+    TheNav
+  }
+}
+</script>

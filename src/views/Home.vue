e3b9b9ac5c6a2d4a64a8095c2e72ef87934e0d14
Asthriona <im@asthriona.com>
2019-10-29 11:55:00 +0100

    add padding youtube player

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,70 @@
 <template>
   <div class="home">
+    <div class="container">
+      <div class="row">
     <TheNav />
+    <ThePlayerCR />
     <ThePlayer />
     <section class="youtube">
       <h2 class="title">YouTube Live Stream</h2>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/56A17bZyNWk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>Cloudsdale Radio</iframe>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/hEHpgI6jBJk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>AnimeFM</iframe>
+      <iframe class="plyerLeft" width="560" height="315" src="https://www.youtube.com/embed/hEHpgI6jBJk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>AnimeFM</iframe>
     </section>
+    </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-    .player {
-        margin-top: 4.3rem;
+    .playerCR {
+        margin-top: 3.3rem;
         padding-top: 2rem;
         padding-bottom: 2rem;    
         z-index: 2;
         position: relative;
-        left: 12%;
+        background-image: url(https://cdn.cloudsdalegroup.com/site/cr/CRbackground.jpg);
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        width: 49%; 
+        min-height: 100px;
+    }
+    .player {
+        margin-top: 3.3rem;
+        padding-right: 10px;
+        padding-top: 2rem;
+        padding-bottom: 2rem;    
+        z-index: 2;
+        position: relative;
+        left: 10px;
         background-image: url(https://cdn.asthriona.com/BeFunky-photo.jpg);
         background-position: center;
         background-repeat: no-repeat;
         background-size: cover;
-        width: 75%; 
+        width: 49%; 
         min-height: 100px;
     }
+    .plyerLeft {
+        position: relative;
+        left: 10px !important;
+    }
+</style>
+<style>
+::-webkit-scrollbar {
+display: none;
+}
 </style>
 
 <script>
 // @ is an alias to /src
 import ThePlayer from '@/components/ThePlayer.vue'
+import ThePlayerCR from '@/components/ThePlayerCR.vue'
 import TheNav from '@/components/TheNav.vue'
 
 export default {
   name: 'home',
   components: {
+    ThePlayerCR,
     ThePlayer,
     TheNav
   },
@@ -43,7 +73,7 @@ export default {
       window.open(
         '/popup/radio',
         'ThePlayer',
-        'TheNav=no, scrollbars=no, width=720, height=154'
+        'TheNav=no, scrollbars=no, width=720, height=261'
       )
     },
   }

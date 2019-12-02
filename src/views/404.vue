19f9c072dc619ce6090048410aefa776edc92c4b
Asthriona <im@asthriona.com>
2019-12-02 14:13:15 +0100

    update 404

--- src/views/404.vue
+++ src/views/404.vue
@@ -3,9 +3,9 @@
   <div class="row">
       <TheNav />
     <div class="mx-auto">
-      <h1 class="text-center"> Not Found </h1>
+      <h1 class="text-center">Page not Found </h1>
       <h3>Did you do smth wrong ?</h3>
-      <p>The server say it dont find the page you ask for, We look everywhere and it seem like we lost it :/ Sorry :c<br>
+      <p>The server said it didnt find the page you ask for, We look everywhere and it seem like we lost it. Sorry :c<br>
       Please try again or try another link, We recommend using the NavBar on the top of you screen. All the link has been verified!</p>
       <p></p>
       <img class="rounded" src="http://cdn.asthriona.com/v9iflkC.gif" alt="idk dude, bouge ta souris! :c">

feb5cedd80f1cb0000b0db6583b511b848ae29b8
Asthriona <im@asthriona.com>
2019-12-02 13:16:56 +0100

    Import axios

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import BootstrapVue from 'bootstrap-vue';
+import axios from 'axios';
 import './registerServiceWorker'
 import 'bootstrap'; 
 import 'bootstrap/dist/css/bootstrap.min.css';

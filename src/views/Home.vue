17a94f91aa83f30890c06fc4a7f2cc56788a359b
Asthriona <im@asthriona.com>
2019-12-12 04:37:35 +0100

    remove player css from home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,21 +29,6 @@
         width: 49%; 
         min-height: 100px;
     }
-    .player {
-        margin-top: 3.3rem;
-        padding-right: 10px;
-        padding-top: 2rem;
-        padding-bottom: 2rem;    
-        z-index: 2;
-        position: relative;
-        left: 10px;
-        background-image: url(https://cdn.asthriona.com/BeFunky-photo.jpg);
-        background-position: center;
-        background-repeat: no-repeat;
-        background-size: cover;
-        width: 49%; 
-        min-height: 100px;
-    }
     .plyerLeft {
         position: relative;
         left: 10px !important;

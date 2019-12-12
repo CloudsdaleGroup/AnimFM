b3ca2bb043ba4130ba8a81e039f7c73a16b3d849
Asthriona <im@asthriona.com>
2019-12-12 06:46:19 +0100

    Disable console loggin for production

--- src/components/ThePlayerCR.vue
+++ src/components/ThePlayerCR.vue
@@ -16,16 +16,12 @@ export default {
         Axios.get('https://broadcaster.animefm.co/api/nowplaying/1')
         .then((response)=>{
             this.meta = response.data.now_playing
-            console.log(response)
-            console.log(this.meta.song.art)
         })
         var refreshMeta = this;
         setInterval(() => {
         Axios.get('https://broadcaster.animefm.co/api/nowplaying/1')
         .then((response)=>{
             this.meta = response.data.now_playing
-            console.log(response)
-            console.log(this.meta.song.art)
         })
         }, 10000); 
     },

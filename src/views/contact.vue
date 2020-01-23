bc25fe66a82eae146d04e27ab3ae35b8848419cb
Asthriona <im@asthriona.com>
2020-01-23 15:58:48 +0100

    Add contact infos

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -1,8 +1,13 @@
 <template>
     <section class="contact">
     <TheNav />
-        <h1>CONTACT PAGE</h1>
-        <i>Soon&#8482;</i>
+        <h1>CONTACT US.</h1>
+        <p><b>How to contact us?</b><br>
+        Sadly for now we don't have any fancy form for now. but you still can contact us by mail at <a href="mailto:contact@AnimeFM.co">Contact@animefm.co</a><br>
+        or for legal information/delettion request <a href="mailto:legal@animefm.co">Legal@animeFM.co</a><br>
+        <b>If you find any issue with the website</b> please create an <a href="https://github.com/CloudsdaleGroup/AnimFM/issues/new" target="_blank" rel="noopener noreferrer">issue</a> on GitHub.
+        </p>
+        <!--<i>Soon&#8482;</i>-->
     </section>
 </template>
 <script>

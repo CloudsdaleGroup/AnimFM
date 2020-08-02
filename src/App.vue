003579121db2b45c7212446bf8843b717ec16db7
Asthriona <im@asthriona.com>
2020-08-02 10:46:04 +0200

    Silent :c

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">  
-    <router-view/>
+    <!--<router-view/>-->
+    <div>
+      <center class="mt-4">
+        <h1>Oops! Looks like we are going silent :/</h1>
+        <p>
+        Hi, I'm Asthriona, Founder and developper of AnimeFM. Due to recent change, we are avtually strugulling with servers.<br>
+        We actually don't have any server to host our broadcaster and it music database. so sadly without broadcaster we are unable to cast any sounds.<br>
+        are are actually in the process to find a place to host our broadcasting system and get back on air as soon as prossible.<br>
+        We let you know on <a href="http://twitter.com/_AnimeFM" target="_blank" rel="noopener noreferrer">Twitter</a> and this website!<br>
+        Please excuse us for the inconvenience
+        </p>
+        &mdash; AnimeFM Dev Team
+      </center>
+    </div>
   <!--<TheFooter />-->
   </div>
 </template>
